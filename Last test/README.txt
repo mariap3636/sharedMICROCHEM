7a71354d68c623392bcc21d430b8fac3fa0e88af
avaclavova <avaclavovaa@gmail.com>
2020-12-01 19:05:44 +0100

    added test of 3 pollutants with new folder names

---
+++ Last test/README.txt
@@ -0,0 +1,8 @@
+Testing folder with complete information for 3 (problematic) pollutants.
+
+The only missing thing is incorporating script addInfoSimcompWithHeader_v2.py into the bash_script.sh,
+it is copied into each folder, but couldn't be launched in each iteration - for some reason this was impossible for me without critical errors in bash script, so I runned it in each folder by hand.
+
+The other scripts and changes are already included in the bash script.
+
+In each folder is file "longname.txt" with the full name of the pollutant.
---
+++ Last test/bash_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+python3 script_for_eawag_01_04_for_testing.py
+
+for i in $( ls -d */); do
+ONE=`echo $i | cut -d'/' -f1`
+echo $ONE;
+MYDIR=$i;#$ONE + "/";
+echo $MYDIR
+
+#done
+cp FetchSEEDSrxnsEAWAG2EdgesListV2.pl $MYDIR;
+cp doTheMapping2SMILESandKEGGcmpIDs.pl $MYDIR;
+cp addInfoSimcompWithHeader_v2.py $MYDIR;
+
+#echo $MYDIR/EAWAGrxns.txt;done
+
+cd $MYDIR;
+cat EAWAGrxns.txt | while read line || [[ -n $line ]];do perl FetchSEEDSrxnsEAWAG2EdgesListV2.pl $line;done
+cat *_cmpds.txt > myEdgeList.csv;
+cat *_enzyme.txt > myEnzymeInfo.csv ;
+cat *AND*.txt > _bothInfo.csv;
+
+FILEbothInfo="_bothInfo.csv";PREFIX=$ONE;mv $FILEbothInfo $PREFIX;
+
+FILE="myEdgeList.csv";
+cut -d " " -f 1 $FILE > c1;
+cut -d " " -f 2 $FILE > c2;
+cat c* | sort | uniq | while read line || [[ -n $line ]];do perl doTheMapping2SMILESandKEGGcmpIDs.pl $line;done
+
+cd "../";done
